2fa0e9330b403b1d733453c3467a6bc3a3d1fa64
Vishwanath R <vishwanath@LogicValley.in>
2021-04-21 14:24:09 +0530

    Commit on completion

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import todo_lists 
+from .views import todo_lists, todo_detail, todo_create, todo_update, todo_delete
 
 app_name = 'todos'
 
 urlpatterns = [
     path('', todo_lists),
+    path('create/', todo_create),
+    path('update/<id>', todo_update),
+    path('delete/<id>', todo_delete),
+    path('<id>/', todo_detail),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Todo
+from .forms import TodoForm
 
 def todo_lists(request):
     todos = Todo.objects.all()
@@ -11,3 +12,43 @@ def todo_lists(request):
     # return HttpResponse("Hello world!")
     # return render(request, 'todo/todo_lists.html')
     return render(request, 'todo/todo_lists.html', context)
+
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        "todo": todo
+    }
+    return render(request, 'todo/todo_detail.html', context)
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        # name = form.cleaned_data['name']
+        # due_date = form.cleaned_data['due_date']
+
+        # new_todo = Todo.objects.create(name=name, due_date=due_date)
+        # pass
+
+        # The above code can be also written as simple as
+        form.save()
+        return redirect('/')
+    context = {
+        "form": form
+    }
+    return render(request, 'todo/todo_create.html', context)
+
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/todo_update.html', context)
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect('/')
